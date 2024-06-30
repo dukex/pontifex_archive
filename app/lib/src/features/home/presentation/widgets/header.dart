8b774e14a21425d9aea932681b4ab13f0bb90a41
Emerson Almeida <emersonalmeidax@gmail.com>
2024-06-30 13:54:30 -0300

    Add Search to Home
    Closes #9

--- app/lib/src/features/home/presentation/widgets/header.dart
+++ app/lib/src/features/home/presentation/widgets/header.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
-
 import 'package:pontifex_archive/i18n.g.dart';
+import 'package:pontifex_archive/src/features/search/presentation/search_bar.dart'
+    as feature;
 
 class Header extends StatelessWidget {
   const Header({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(context.t.home.header.title,
-        style: Theme.of(context)
-            .textTheme
-            .displayMedium
-            ?.copyWith(fontWeight: FontWeight.bold));
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      Text(context.t.home.header.title,
+          style: Theme.of(context)
+              .textTheme
+              .displayMedium
+              ?.copyWith(fontWeight: FontWeight.bold)),
+      const feature.SearchBar(),
+    ]);
   }
 }
