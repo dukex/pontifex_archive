1eca3b8c344bfbcd556a258f0533410c567c76a7
Emerson Almeida <emersonalmeidax@gmail.com>
2024-07-27 01:27:15 -0300

    add more popes and more documents
    Ref #4

--- epub_builder/epub_builder/builder.py
+++ epub_builder/epub_builder/builder.py
@@ -5,6 +5,7 @@ from .repositories import structures;
 from .scrapers.vatican import EncyclicalLetterScraper;
 from . import epub
 import os
+import re
 import sqlite3
 
 def generate():
@@ -20,62 +21,114 @@ def generate():
 
 
     _popes = popes.all()
-    # for pope in _popes:
-    #     for document in pope.documents:
-    #         document.type = "enc"
-    #         document.author_id = pope.id
-    #         for document_translation in document.translations:
-    #             document_translation.id = f"{document_translation.language_code}/{document.id}"
-    #             document_translation.epub_url = f"https://pontifexarchive.leigo.fm/api/popes/{pope.id}/{document_translation.id}.epub"
+    for pope in _popes:
+        for document in pope.documents:
+            document.type = "enc"
+            document.author_id = pope.id
+            for document_translation in document.translations:
+                document_translation.id = f"{document_translation.language_code}/{document.id}"
+                document_translation.epub_url = f"https://pontifexarchive.leigo.fm/api/popes/{pope.id}/{document_translation.id}.epub"
 
-    #             scraper = EncyclicalLetterScraper(document.name, document_translation.vatican_url, pope.signatures)
-    #             scraper.run()
+                scraper = EncyclicalLetterScraper(document.name, document_translation.vatican_url, pope.signatures)
+                scraper.run()
 
-    #             scraper.index_it(cur, document, document_translation)
+                scraper.index_it(cur, document, document_translation)
 
-    #             structures.save(document_translation, scraper.structure)
-    #             epub.create(pope, document, document_translation, scraper.structure)
+                structures.save(document_translation, scraper.structure)
+                epub.create(pope, document, document_translation, scraper.structure)
 
-    for url in [
-        "https://www.vatican.va/content/pius-xii/en.html",
-        "https://www.vatican.va/content/pius-xi/en.html",
-        "https://www.vatican.va/content/benedict-xv/en.html",
-        "https://www.vatican.va/content/pius-x/en.html",
-        "https://www.vatican.va/content/leo-xiii/en.html"
-    ]:
+        url = f"https://www.vatican.va/content/{pope.id}/en/encyclicals.index.html"
         root = _rootElement(url);
-        infos = root.find_all('h2');
+        if root is not None:
+            for document_root in root.select('li .item'):
+                h1 = document_root.select_one('h1')
+                translations = [{
+                        "language_code": to_language(l.text),
+                        "vatican_url": f"https://www.vatican.va{l['href']}"
+                    } for l in document_root.select('h2 a')];
 
-        s_date =  infos[1].text.split(".")
+                title = re.search(r'^ ?(.+) \((.+) (\d{,2}), (\d{4})\)', h1.text.replace('\xa0', ' '))
 
-        pope =  {
-            "number": 1111111,
-            "id": "ioannes-pp-xxiii",
-            "image_url": "",
-            "name": root.find('h1').text,
-            "personal_name": infos[0].text,
-            "motto": "",
-            "country": "",
-            "start_date": s_date,
-            "end_date": infos[2].text.split("."),
-            "signatures": [],
-            "translations": [],
-            "documents": []
-        }
-        print(pope)
-
-
+                if title:
+                    data = [group.replace('\r', '') for group in title.groups()]
+                    if len(translations) > 0:
+                        url = translations[0]['vatican_url']
+                        url_parts = url.split("_")
+                        id_part = url_parts[len(url_parts) - 1].replace('.html', '')
+                        d_id = f"enc-{data[3]}{to_month(data[1])}{data[2]}-{id_part}"
+                        if len([d for d in pope.documents if d.id == d_id]) == 0:
+                            pope.documents.append({
+                                "id": d_id,
+                                "authorId": pope.id,
+                                "name": data[0],
+                                "date": f"{data[3]}-{to_month(data[1])}-{data[2]}",
+                                "type": "enc",
+                                "translations": translations,
+                                "cover_url": "https://pontifexarchive.leigo.fm/api/covers/fallback.png",
+                                "author_id": pope.id
+                            })
+                else:
+                    print(h1)
 
     con.commit()
     cur.close()
     popes.save(_popes)
 
+def to_language(language):
+    if language == 'English':
+        return 'en'
+    elif language == 'Italian':
+        return 'it'
+    elif language == 'Latin':
+        return 'la'
+    elif language == 'Portuguese':
+        return 'pt'
+    elif language == 'French':
+        return 'fr'
+    elif language == 'German':
+        return 'de'
+    elif language == 'Spanish':
+        return 'es'
+    elif language == 'Hungarian':
+        return 'hu'
+    else:
+        raise BaseException(f"error '{language}'")
+
+def to_month(month):
+    return {
+        'January': '01',
+        'February': '02',
+        'March': '03',
+        'April': '04',
+        'May': '05',
+        'June': '06',
+        'July': '07',
+        'August': '08',
+        'September': '09',
+        'October': '10',
+        'November': '11',
+        'December': '12'
+    }[month.replace(r' ','')]
 
 def _rootElement(url):
-    return _buildSoup(url).select_one(".infos .info")
+    return _buildSoup(url).select_one(".documento .vaticanindex")
 
 def _buildSoup(url):
     return BeautifulSoup(_fetchHTML(url), 'html.parser')
 
 def _fetchHTML(url):
     return requests.get(url).content
+
+
+def roman_to_i(s):
+    roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+    i = 0
+    num = 0
+    while i < len(s):
+        if i+1<len(s) and s[i:i+2] in roman:
+            num+=roman[s[i:i+2]]
+            i+=2
+        else:
+            num+=roman[s[i]]
+            i+=1
+    return num if num > 9 else f"0{num}"
